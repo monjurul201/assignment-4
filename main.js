cf1367d643411c18b2fea2cbe2c8d34b82744c2d
unknown <mojnushah2018@gmai.com>
2021-01-30 10:35:32 +0600

    js complete

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
-function ticketQuantityChange(ticket, isIncrement){
-    const ticketCount=document.getElementById(ticket +'-Count');
-    const TicketCount=parseInt(ticketCount.value);
-    let ticketNewCount=TicketCount;
-    if(isIncrement==true){
-         ticketNewCount= TicketCount+1;
+function ticketQuantityChange(ticket, isIncrement) {
+    const ticketCount = document.getElementById(ticket + '-Count');
+    const TicketCount = parseInt(ticketCount.value);
+    let ticketNewCount = TicketCount;
+    if (isIncrement == true) {
+        ticketNewCount = TicketCount + 1;
     }
-    if(isIncrement==false &&  TicketCount>0){
-         ticketNewCount= TicketCount-1;     
+    if (isIncrement == false && TicketCount > 0) {
+        ticketNewCount = TicketCount - 1;
     }
-   ticketCount.value= ticketNewCount;
-   let ticketTotal=0;
-   if(ticket=='special'){
-       ticketTotal=ticketNewCount*150;
-   }
-   if(ticket=='common'){
-       ticketTotal=ticketNewCount*100;
-   }
+    ticketCount.value = ticketNewCount;
+    let ticketTotal = 0;
+    if (ticket == 'special') {
+        ticketTotal = ticketNewCount * 150;
+    }
+    if (ticket == 'common') {
+        ticketTotal = ticketNewCount * 100;
+    }
 
-    document.getElementById(ticket + '-totalTicketAmount').innerText=ticketTotal;
-    
+    document.getElementById(ticket + '-totalTicketAmount').innerText = ticketTotal;
+    calculationTotal();
 }
-// function calculationTotal (){
-// const specialCount=document.getElementById('special-Count').value;
-// const specialCount=parseInt(specialCount.value);  
+function calculationTotal() {
+    const specialCount = getInputValue('special');
+    const commonCount = getInputValue('common');
 
-// const commonCount=document.getElementById('common-Count').value;
-// const commonCount=parseInt(commonCount.value);
-// const Subtotal=specialCount*150+commonCount*100;
-// document.getElementById('SubTotal-prize').innerText=Subtotal;
-// }
+    const Subtotal = specialCount * 150 + commonCount * 100;
+    document.getElementById('SubTotal-prize').innerText = Subtotal;
+    const tax = Math.round(Subtotal * 0.1);
+    document.getElementById('tax-amount').innerText = tax;
+
+    const grandTotal = Subtotal + tax;
+    document.getElementById('grand-total').innerText = grandTotal;
+
+}
+
+function getInputValue(ticket) {
+    const ticketInput = document.getElementById(ticket + '-Count');
+    const ticketCount = parseInt(ticketInput.value);
+    return ticketCount
+}
 
 
 //    function specialQuantityChange(isIncrement){
